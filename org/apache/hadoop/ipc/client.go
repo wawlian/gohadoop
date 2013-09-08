f64a92a2619f848b4f17818b14a69adf52c3e65a
Arun C. Murthy <acmurthy@apache.org>
2013-09-08 02:41:37 -0700

    Making progress on UUID, connection setup

--- org/apache/hadoop/ipc/client.go
+++ org/apache/hadoop/ipc/client.go
@@ -1,11 +1,45 @@
 package ipc 
 
-import "fmt"
+import (
+  "fmt" 
+  "bytes"
+  "net"
+  "github.com/nu7hatch/gouuid"
+)
 
 type Client struct {
+  ClientId *uuid.UUID
+  Server string
+  Port int
 }
 
-func call(c Client) {
-  fmt.Printf("Client.call")
+func (c *Client) String() string {
+  buf := bytes.NewBufferString("Client: <")
+  fmt.Fprint(buf, " ", c.ClientId)
+  fmt.Fprint(buf, " ", c.Server)
+  fmt.Fprint(buf, ":")
+  fmt.Fprint(buf, c.Port)
+  fmt.Fprint(buf, " >");
+  return buf.String()
+
 }
 
+func Call(c *Client) {
+  fmt.Println(c)
+  fmt.Println("Connecting...")
+  _, err := getConnection(c)
+  if err != nil {
+    fmt.Println("error")
+  } else {
+    fmt.Println("success")
+  }
+}
+
+func getConnection (c *Client) (net.Conn, error) {
+  buf := bytes.NewBufferString("")
+  fmt.Fprint(buf, c.Server)
+  fmt.Fprint(buf, ":")
+  fmt.Fprint(buf, c.Port)
+  
+  return net.Dial("tcp", buf.String())
+}
