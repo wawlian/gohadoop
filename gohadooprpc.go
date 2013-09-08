57eb5fb8e9f05081c491c7a80416ea5b49e89d15
Arun C. Murthy <acmurthy@apache.org>
2013-09-08 12:44:04 -0700

    Ok, now up against protobuf... so far, so good.

--- gohadooprpc.go
+++ gohadooprpc.go
@@ -1,7 +1,52 @@
 package gohadooprpc 
 
-import "fmt"
+import (
+  "bytes"
+  "encoding/binary"
+)
 
-func main() {
-  fmt.Printf("hello, world")
+var RPC_HEADER []byte = []byte("hrpc") 
+var VERSION []byte = []byte {9} 
+var RPC_SERVICE_CLASS int32 = 1
+
+type AuthMethod byte
+const (
+  AUTH_SIMPLE AuthMethod = 80
+  AUTH_KERBEROS AuthMethod = 81
+  AUTH_TOKEN AuthMethod = 82
+  AUTH_PLAIN AuthMethod = 83
+)
+func (authmethod AuthMethod) String() string {
+  switch {
+    case authmethod == AUTH_SIMPLE:
+      return "SIMPLE"
+    case authmethod == AUTH_KERBEROS:
+      return "GSSAPI"
+    case authmethod == AUTH_TOKEN:
+      return "DIGEST-MD5"
+    case authmethod == AUTH_PLAIN:
+      return "PLAIN"
+  }
+  return "ERROR-UNKNOWN"
+}
+
+type AuthProtocol int32 
+const (
+  AUTH_PROTOCOL_NONE AuthProtocol = 0
+  AUTH_PROTOCOL_SASL AuthProtocol = -33
+)
+func (authprotocol AuthProtocol) String() string {
+  switch {
+    case authprotocol == AUTH_PROTOCOL_NONE:
+      return "NONE"
+    case authprotocol == AUTH_PROTOCOL_SASL:
+      return "SASL"
+  }
+  return "ERROR-UNKNOWN"
+}
+
+func ConvertFixedToBytes (data interface{}) ([]byte, error) {
+  buf := new(bytes.Buffer)
+  err := binary.Write(buf, binary.BigEndian, data)
+  return buf.Bytes(), err
 }
--- org/apache/hadoop/ipc/client.go
+++ org/apache/hadoop/ipc/client.go
@@ -3,43 +3,103 @@ package ipc
 import (
   "fmt" 
   "bytes"
+  "log"
   "net"
+  "strconv"
   "github.com/nu7hatch/gouuid"
+  "github.com/gohadooprpc"
 )
 
 type Client struct {
   ClientId *uuid.UUID
   Server string
   Port int
+  TCPNoDelay bool
 }
 
-func (c *Client) String() string {
-  buf := bytes.NewBufferString("Client: <")
-  fmt.Fprint(buf, " ", c.ClientId)
-  fmt.Fprint(buf, " ", c.Server)
-  fmt.Fprint(buf, ":")
-  fmt.Fprint(buf, c.Port)
-  fmt.Fprint(buf, " >");
+type connection struct {
+  con *net.TCPConn 
+}
+
+func (c *Client) String() (string) {
+  buf := bytes.NewBufferString("")
+  fmt.Fprint(buf, "<clientId:", c.ClientId)
+  fmt.Fprint(buf, ", server:", getServerAddr(c)) 
+  fmt.Fprint(buf, ">");
   return buf.String()
 
 }
 
-func Call(c *Client) {
-  fmt.Println(c)
-  fmt.Println("Connecting...")
+func (c *Client) Call () (error) {
+  log.Println("Connecting...", c)
   _, err := getConnection(c)
-  if err != nil {
-    fmt.Println("error")
-  } else {
-    fmt.Println("success")
-  }
+  return err
 }
 
-func getConnection (c *Client) (net.Conn, error) {
-  buf := bytes.NewBufferString("")
-  fmt.Fprint(buf, c.Server)
-  fmt.Fprint(buf, ":")
-  fmt.Fprint(buf, c.Port)
+func getServerAddr (c *Client) (string) {
+  return net.JoinHostPort(c.Server, strconv.Itoa(c.Port))
+}
+
+func getConnection (c *Client) (connection, error) {
+  con, err := setupConnection(c)
+  writeConnectionHeader(con)
+  writeConnectionContext(con)
+  return con, err
+}
+
+func setupConnection (c *Client) (connection, error) {
+  addr, _ := net.ResolveTCPAddr("tcp", getServerAddr(c))
+  tcpConn, err := net.DialTCP("tcp", nil, addr) 
+  if err != nil {
+    log.Println("error: ", err)
+    return connection{}, err
+  } else {
+    log.Println("Successfully connected ", c)
+  }
+
+  // Set tcp no-delay
+  tcpConn.SetNoDelay(c.TCPNoDelay)
+
+  return connection{tcpConn}, nil 
+}
+
+func writeConnectionHeader (conn connection) (error) {
+  // RPC_HEADER
+  log.Println("conn.Write: ", gohadooprpc.RPC_HEADER)
+  if _, err := conn.con.Write(gohadooprpc.RPC_HEADER); err != nil {
+    log.Fatal("conn.Write", err)
+    return err
+  }
   
-  return net.Dial("tcp", buf.String())
+
+  // RPC_VERSION
+  log.Println("conn.Write: ", gohadooprpc.RPC_HEADER)
+  if _, err := conn.con.Write(gohadooprpc.VERSION); err != nil {
+    log.Fatal("conn.Write", err)
+    return err
+  }
+
+  // RPC_SERVICE_CLASS
+  if serviceClass, err := gohadooprpc.ConvertFixedToBytes(gohadooprpc.RPC_SERVICE_CLASS); err != nil {
+    log.Fatal("WTF binary.Write", err)
+    return err
+  } else if _, err := conn.con.Write(serviceClass); err != nil {
+    log.Fatal("conn.Write RPC_SERVICE_CLASS", err)
+    return err
+  }
+
+  // AuthProtocol
+  if authProtocol, err := gohadooprpc.ConvertFixedToBytes(gohadooprpc.AUTH_PROTOCOL_NONE); err != nil {
+    log.Fatal("WTF AUTH_PROTOCOL_NONE", err)
+    return err
+  } else if _, err := conn.con.Write(authProtocol); err != nil {
+    log.Fatal("conn.Write RPC_SERVICE_CLASS", err)
+    return err
+  }
+
+  return nil 
+}
+
+func writeConnectionContext (conn connection) (error) {
+  return nil
 }
