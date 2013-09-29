5285bf32acba640a016dd606258fb6c0b23e3695
tim <tim@fewagainstmany.com>
2013-09-29 10:59:19 -0700

    fixing broken path + go fmt

--- driver/client_driver.go
+++ driver/client_driver.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-  "log"
-  "github.com/gohadoop"
-  "github.com/gohadoop/hadoop_common/ipc/client"
-  "github.com/gohadoop/hadoop_common"
-  "github.com/gohadoop/hadoop_yarn"
-  "github.com/nu7hatch/gouuid"
+	"github.com/gohadoop"
+	"github.com/hortonworks/gohadoop/hadoop_common"
+	"github.com/hortonworks/gohadoop/hadoop_common/ipc/client"
+	"github.com/hortonworks/gohadoop/hadoop_yarn"
+	"github.com/nu7hatch/gouuid"
+	"log"
 )
 
 func main() {
-  var err error
+	var err error
 
-  clientId, _ := uuid.NewV4()
-  ugi, _ := gohadoop.CreateSimpleUGIProto()
-  c := &ipc.Client{ClientId: clientId, Ugi: ugi, ServerAddress: "0.0.0.0:28081"}
-  var clientProtocolVersion uint64 = 1
-  var methodName string
-  var protocolName string
+	clientId, _ := uuid.NewV4()
+	ugi, _ := gohadoop.CreateSimpleUGIProto()
+	c := &ipc.Client{ClientId: clientId, Ugi: ugi, ServerAddress: "0.0.0.0:28081"}
+	var clientProtocolVersion uint64 = 1
+	var methodName string
+	var protocolName string
 
-  // ApplicationClientProtocol.getApplications 
-  methodName = "getApplications"
-  protocolName = "org.apache.hadoop.yarn.api.ApplicationClientProtocolPB"
-  getAppsRpcProto := hadoop_common.RequestHeaderProto {MethodName: &methodName, DeclaringClassProtocolName: &protocolName, ClientProtocolVersion: &clientProtocolVersion}
-  applicationStates := []hadoop_yarn.YarnApplicationStateProto{hadoop_yarn.YarnApplicationStateProto_ACCEPTED, hadoop_yarn.YarnApplicationStateProto_RUNNING, hadoop_yarn.YarnApplicationStateProto_SUBMITTED}
-  getAppsReqProto := hadoop_yarn.GetApplicationsRequestProto {ApplicationStates: applicationStates}
-  getAppsResProto := hadoop_yarn.GetApplicationsResponseProto{}
-  log.Println("Calling rpc method: ", methodName) 
-  err = c.Call(&getAppsRpcProto, &getAppsReqProto, &getAppsResProto)
-  if err != nil {
-    log.Fatal("Client.call failed", err)
-  }
-  log.Println("Returned response: ", getAppsResProto)
+	// ApplicationClientProtocol.getApplications
+	methodName = "getApplications"
+	protocolName = "org.apache.hadoop.yarn.api.ApplicationClientProtocolPB"
+	getAppsRpcProto := hadoop_common.RequestHeaderProto{MethodName: &methodName, DeclaringClassProtocolName: &protocolName, ClientProtocolVersion: &clientProtocolVersion}
+	applicationStates := []hadoop_yarn.YarnApplicationStateProto{hadoop_yarn.YarnApplicationStateProto_ACCEPTED, hadoop_yarn.YarnApplicationStateProto_RUNNING, hadoop_yarn.YarnApplicationStateProto_SUBMITTED}
+	getAppsReqProto := hadoop_yarn.GetApplicationsRequestProto{ApplicationStates: applicationStates}
+	getAppsResProto := hadoop_yarn.GetApplicationsResponseProto{}
+	log.Println("Calling rpc method: ", methodName)
+	err = c.Call(&getAppsRpcProto, &getAppsReqProto, &getAppsResProto)
+	if err != nil {
+		log.Fatal("Client.call failed", err)
+	}
+	log.Println("Returned response: ", getAppsResProto)
 
-  // ApplicationClientProtocol.getNewApplication
-  methodName = "getNewApplication"
-  protocolName = "org.apache.hadoop.yarn.api.ApplicationClientProtocolPB"
-  getNewAppRpcProto := hadoop_common.RequestHeaderProto {MethodName: &methodName, DeclaringClassProtocolName: &protocolName, ClientProtocolVersion: &clientProtocolVersion}
-  getNewAppReqProto := hadoop_yarn.GetNewApplicationRequestProto {}
-  getNewAppResProto := hadoop_yarn.GetNewApplicationResponseProto {}
-  log.Println("Calling rpc method: ", methodName) 
-  err = c.Call(&getNewAppRpcProto, &getNewAppReqProto, &getNewAppResProto)
-  if err != nil {
-    log.Fatal("Client.call failed", err)
-  }
-  log.Println("Returned response: ", getNewAppResProto)
+	// ApplicationClientProtocol.getNewApplication
+	methodName = "getNewApplication"
+	protocolName = "org.apache.hadoop.yarn.api.ApplicationClientProtocolPB"
+	getNewAppRpcProto := hadoop_common.RequestHeaderProto{MethodName: &methodName, DeclaringClassProtocolName: &protocolName, ClientProtocolVersion: &clientProtocolVersion}
+	getNewAppReqProto := hadoop_yarn.GetNewApplicationRequestProto{}
+	getNewAppResProto := hadoop_yarn.GetNewApplicationResponseProto{}
+	log.Println("Calling rpc method: ", methodName)
+	err = c.Call(&getNewAppRpcProto, &getNewAppReqProto, &getNewAppResProto)
+	if err != nil {
+		log.Fatal("Client.call failed", err)
+	}
+	log.Println("Returned response: ", getNewAppResProto)
 }
--- driver/config_driver.go
+++ driver/config_driver.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-  "log"
-  hadoop_conf "github.com/gohadoop/hadoop_common/conf"
-  yarn_conf "github.com/gohadoop/hadoop_yarn/conf"
+	hadoop_conf "github.com/hortonworks/gohadoop/hadoop_common/conf"
+	yarn_conf "github.com/hortonworks/gohadoop/hadoop_yarn/conf"
+	"log"
 )
 
 func main() {
-  
-  conf, _ := hadoop_conf.NewConfigurationResources([]string{yarn_conf.YARN_DEFAULT, yarn_conf.YARN_SITE})
 
-  fsName, _ := conf.Get("fs.default.name", "XXX")
-  log.Println("fs.default.name = ", fsName) 
+	conf, _ := hadoop_conf.NewConfigurationResources([]string{yarn_conf.YARN_DEFAULT, yarn_conf.YARN_SITE})
 
-  cores, _ := conf.GetInt("yarn.nodemanager.resource.cpu-cores", -1)
-  log.Println("yarn.nodemanager.resource.cpu-cores = ", cores) 
+	fsName, _ := conf.Get("fs.default.name", "XXX")
+	log.Println("fs.default.name = ", fsName)
+
+	cores, _ := conf.GetInt("yarn.nodemanager.resource.cpu-cores", -1)
+	log.Println("yarn.nodemanager.resource.cpu-cores = ", cores)
 }
-
