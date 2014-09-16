88b2c39ffc809e7096fb8b0b9137cb3756e56de7
Sidharta Seethana <sidharta.apache@gmail.com>
2014-09-16 16:17:12 -0700

    First cut impl. for UserGroupInformation.go

---
+++ hadoop_common/UserGroupInformation.go
@@ -0,0 +1,86 @@
+package hadoop_common
+
+import (
+//  "fmt"
+  "sync"
+  "log"
+  "os/user"
+)
+
+
+/** a (very) basic UserGroupInformation implementation for storing user data/tokens,
+  This implementation is currently *not* thread-safe
+ */
+
+type UserGroupInformation struct {
+  rwMutex sync.RWMutex
+  userInfo  *UserInformationProto
+  userTokens []TokenProto
+}
+
+var once sync.Once
+var currentUserGroupInformation *UserGroupInformation
+var maxTokens = 16
+
+func CreateCurrentUserInfoProto() (*UserInformationProto, error) {
+  // Figure the current user-name
+  var username string
+  if currentUser, err := user.Current(); err != nil {
+    log.Fatal("user.Current", err)
+    return nil, err
+  } else {
+    username = currentUser.Username
+  }
+
+  return &UserInformationProto{EffectiveUser: nil, RealUser: &username}, nil
+}
+
+
+func Allocate(userInfo *UserInformationProto, userTokens []TokenProto) (*UserGroupInformation) {
+  ugi := new(UserGroupInformation)
+
+  if(userInfo != nil) {
+    ugi.userInfo = userInfo;
+  } else {
+    currentUserInfo, _ := CreateCurrentUserInfoProto();
+    ugi.userInfo = currentUserInfo;
+  }
+
+  if(userTokens != nil) {
+    ugi.userTokens = userTokens;
+  } else {
+    ugi.userTokens = make([]TokenProto, 0, maxTokens);  //empty, with room for maxTokens tokens.
+  }
+
+  return ugi;
+}
+
+
+func initializeCurrentUser() {
+  once.Do(func() {
+    currentUserGroupInformation = Allocate(nil, nil)
+  })
+}
+
+func (ugi *UserGroupInformation) GetUserInformation () (*UserInformationProto) {
+  return ugi.userInfo
+}
+
+func (ugi *UserGroupInformation) GetUserTokens () ([]TokenProto) {
+  return ugi.userTokens
+}
+
+func (ugi* UserGroupInformation) AddUserToken(token *TokenProto) {
+  if length := len(ugi.userTokens); (length < maxTokens) {
+    //ref: http://blog.golang.org/slices
+    ugi.userTokens = append(ugi.userTokens,*token)
+  } else {
+    log.Fatal("user already has maxTokens:",maxTokens);
+  }
+}
+
+func GetCurrentUser() (*UserGroupInformation) {
+  initializeCurrentUser()
+
+  return currentUserGroupInformation
+}
