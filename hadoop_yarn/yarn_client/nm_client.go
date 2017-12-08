2e6ad4b98ae63bf927dbb4504d6afab8dfec79f8
籁鸣 <jerry.xf@alibaba-inc.com>
2017-12-08 14:14:52 +0800

    Add protocol nodemanager_resource_utilization_tracker.proto, related service code to communicate with nodemanager. This commit provides the functionality to get node status of a nodemanager. A demo nm_example.go is also provided for demostrate the usage.

---
+++ hadoop_yarn/yarn_client/nm_client.go
@@ -0,0 +1,23 @@
+package yarn_client
+
+import (
+	"github.com/hortonworks/gohadoop/hadoop_yarn"
+	yarn_conf "github.com/hortonworks/gohadoop/hadoop_yarn/conf"
+)
+
+type NMClient struct {
+	client hadoop_yarn.NodeManagerResourceUtilizationTrackerService
+	conf   yarn_conf.YarnConfiguration
+}
+
+func CreateNMClient(conf yarn_conf.YarnConfiguration) (*NMClient, error) {
+	c, err := hadoop_yarn.DialNodeManagerResourceUtilizationTrackerService(conf)
+	return &NMClient{client: c, conf: conf}, err
+}
+
+func (client *NMClient) GetResourceUtilization() (*hadoop_yarn.ResourceUtilizationResponseProto,error) {
+	req := &hadoop_yarn.ResourceUtilizationRequestProto{}
+	resp := &hadoop_yarn.ResourceUtilizationResponseProto{}
+	return resp, client.client.GetResourceUtilization(req, resp)
+}
+
